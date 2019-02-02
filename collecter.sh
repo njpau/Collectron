58cdb2c1f7b1347a974c301e21f511cca0fa1292
njpau <nikiljames.paul@griffithuni.edu.au>
2019-02-02 16:42:53 +1000

    Added code to remove duplicate user names and email

--- collecter.sh
+++ collecter.sh
@@ -13,13 +13,17 @@ do
 	git log > ../${arr[3]}_log.txt
 	cd $path
 	rm -rf $appPath
+	#read authors
 	while read line
 	do
 		if [[ $line = "Author: "* ]]; then
-
+			if [[ $(grep "$line" "Developers.txt") ]]; then
+				echo "Developer name already saved"
+			else
 				echo "Writing Developer info to file"
 				echo $line >> Developers.txt
+			fi
 		fi
 	done < ${arr[3]}_log.txt
 
-done < gitlinks.txt
+	done < gitlinks.txt
